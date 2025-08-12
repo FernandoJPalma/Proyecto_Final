4b4299b9e7e09ed2d98e4f9975f1690eb3e09f26
Alejandro <achavezi@unah.hn>
2025-08-12 12:57:18 -0600

    Cambio al provider para retornar un List<Medicamento>

--- proyecto_final/lib/controllers/medicamentos_controller.dart
+++ proyecto_final/lib/controllers/medicamentos_controller.dart
@@ -6,7 +6,6 @@ import 'package:proyecto_final/models/medicamento_provider.dart';
 class MedicamentosController extends GetxController {
   final GetStorage _storage = GetStorage();
   final RxList<Medicamento> medicamentos = <Medicamento>[].obs;
-  
 
   @override
   void onInit() async {
@@ -14,23 +13,23 @@ class MedicamentosController extends GetxController {
     cargarMedicamentosLocal();
 
     try {
+      final cargadoDesdeJson = _storage.read('cargadoDesdeJson') ?? false;
+      if (!cargadoDesdeJson) {
+        final jsonList = await MedicamentoProvider()
+            .getMedicamentos()
+            .catchError((e) {
+              Get.snackbar('Error', 'Fallo al cargar desde API: $e');
+              return <Medicamento>[];
+            });
 
-    final cargadoDesdeJson = _storage.read('cargadoDesdeJson') ?? false;
-    if (!cargadoDesdeJson) {
-      final jsonList = await MedicamentoProvider().getMedicamentos()
-          .catchError((e) {
-            Get.snackbar('Error', 'Fallo al cargar desde API: $e');
-            return <dynamic>[];
-          });
-
-      if (jsonList.isNotEmpty) {
-        cargarMedicamentosDelJson(jsonList);
-        _storage.write('cargadoDesdeJson', true);
+        if (jsonList.isNotEmpty) {
+          cargarMedicamentosDelJson(jsonList);
+          _storage.write('cargadoDesdeJson', true);
+        }
       }
+    } catch (e) {
+      Get.log('Error en el onInit: $e', isError: true);
     }
-  } catch (e) {
-    Get.log('Error en el onInit: $e', isError: true);
-  }
   }
 
   void cargarMedicamentosLocal() {
@@ -44,10 +43,8 @@ class MedicamentosController extends GetxController {
   }
 
   void agregarMedicamento(Medicamento med) {
-    print(_storage.read('medicamentos'));
     medicamentos.add(med);
     _storage.write('medicamentos', medicamentos);
-    print(medicamentos);
   }
 
   void removeItem(int index) {
@@ -56,46 +53,20 @@ class MedicamentosController extends GetxController {
   }
 
   void actualizarMedicamento(int index, Medicamento nuevosDatos) {
-  if (index >= 0 && index < medicamentos.length) {
-    medicamentos[index] = nuevosDatos;
-    _storage.write('items', medicamentos); // Si usas almacenamiento local
-    update(); // Notificar a los listeners
+    if (index >= 0 && index < medicamentos.length) {
+      medicamentos[index] = nuevosDatos;
+      _storage.write('items', medicamentos); // Si usas almacenamiento local
+      update(); // Notificar a los listeners
+    }
   }
-}
 
-  void cargarMedicamentosDelJson(List<dynamic> jsonList) {
-  try {
-    final List<Medicamento> loadedMedicamentos = jsonList
-        .map((json) {
-          try {
-            return Medicamento.fromJson(json);
-          } catch (e) {
-            throw 'Error en formato JSON del medicamento: $e';
-          }
-        })
-        .toList();
-
-    medicamentos.addAll(loadedMedicamentos);
-    _storage.write('medicamentos', medicamentos);
-  } catch (e) {
-    Get.snackbar('Error', 'No se pudieron cargar los medicamentos: $e');
-    rethrow;
+  void cargarMedicamentosDelJson(List<Medicamento> medicamentoList) {
+    try {
+      medicamentos.addAll(medicamentoList);
+      _storage.write('medicamentos', medicamentos);
+    } catch (e) {
+      Get.snackbar('Error', 'No se pudieron cargar los medicamentos: $e');
+      rethrow;
+    }
   }
-}
-
-  // final _listaMedicamentos = <Medicamento>[].obs;
-  //   void agregarMedicamento(Medicamento nuevomd) {
-  //   final medicamentos = obtenerMedicamento();
-  //   if (obtenerMedicamento()==null) {
-  //     storage.write('medicamentos', [nuevomd]);
-  //     // print(storage.read('medicamentos'));
-  //   }else{
-  //     medicamentos?.add(nuevomd);
-  //     storage.write('medicamentos', medicamentos);
-  //   }
-  // }
-
-  // List<Medicamento>? obtenerMedicamento() {
-  //   return storage.read('medicamentos');
-  // }
 }
--- proyecto_final/lib/models/medicamento_provider.dart
+++ proyecto_final/lib/models/medicamento_provider.dart
@@ -3,18 +3,25 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class MedicamentoProvider {
-  final String _url = "https://689ae8cae727e9657f630064.mockapi.io/Medicamentos";
+import 'package:proyecto_final/models/medicamento.dart';
 
-  Future<List<dynamic>> getMedicamentos() async {
+class MedicamentoProvider {
+  final String _url =
+      "https://689ae8cae727e9657f630064.mockapi.io/Medicamentos";
+
+  Future<List<Medicamento>> getMedicamentos() async {
     try {
-      final response = await http.get(Uri.parse(_url)).timeout(
-        const Duration(seconds: 10),
-        onTimeout: () => throw TimeoutException('Tiempo de espera agotado'),
-      );
+      final response = await http
+          .get(Uri.parse(_url))
+          .timeout(
+            const Duration(seconds: 10),
+            onTimeout: () => throw TimeoutException('Tiempo de espera agotado'),
+          );
 
       if (response.statusCode == 200) {
-        return json.decode(response.body);
+        return List<Medicamento>.from(
+          json.decode(response.body).map((c) => Medicamento.fromJson(c)),
+        );
       } else {
         throw HttpException('Error HTTP ${response.statusCode}');
       }
@@ -26,7 +33,4 @@ class MedicamentoProvider {
       throw 'Error al cargar medicamentos: ${e.toString()}';
     }
   }
-   
-
 }
-
